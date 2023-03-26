28c7cb2257a5d295885db145872fe1cde99da2c8
FrancoYudica <francoyudica@gmail.com>
2023-03-26 19:01:37 -0300

    TP complejidad

---
+++ practicas/tp-complejidad/code/ejercicio5.py
@@ -0,0 +1,5 @@
+"""
+Implementar un algoritmo Contiene-Suma(A,n) que recibe una
+lista de enteros A y un entero n y devuelve True si existen en
+A un par de elementos que sumados den n. Analice el costo computacional.
+"""
