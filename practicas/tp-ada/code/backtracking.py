ac166648e824a193738304028a52b8d6796112b6
FrancoYudica <francoyudica@gmail.com>
2023-05-26 11:49:41 -0300

    Backtracking

---
+++ practicas/tp-ada/code/backtracking.py
@@ -0,0 +1,124 @@
+
+
+def give_change(change, coins):
+    # change is the amount
+    # coins is an unsorted array containing all the possible coin types
+
+    def _give_recursive(remaining_change, sorted_coins):
+        # remaining_change: is the remaining value to get the change
+        # sorted_coins: the available coins sorted in increasing order
+
+        min_branch_count = float("inf")
+        min_branch_coins = []
+
+        for coin in reversed(sorted_coins):
+
+            # 1) Can't use the coin - cuts the branch     
+            if remaining_change < coin:
+                continue
+            
+            # 2) When the coin is the exact remaining value
+            if remaining_change == coin:
+                return [coin]
+            
+            # 3) Finds the branch with the min coins count
+            branch_coins = _give_recursive(remaining_change - coin, sorted_coins)
+
+            if len(branch_coins) < min_branch_count:
+                min_branch_count = len(branch_coins)
+
+                # Concatenate the current coin with the branch
+                min_branch_coins = [coin] + branch_coins
+
+        return min_branch_coins
+
+    return _give_recursive(change, sorted(coins))
+
+
+def maximize_bag_weight(max_weight, cans_weight: list):
+    # Maximizes the weight, trying to minimize the amount of cans. That's why sorting is used
+    # Note that the max_weight isn't necessarily reached
+
+    def _bag_recursive(remaining_weight, remaining_cans):
+        
+        max_weight_reached = -1
+        max_weight_cans = []
+
+        for can_weight in reversed(remaining_cans):
+            
+            # 1) Can't add to the bag - cuts branch
+            if can_weight > remaining_weight:
+                continue
+            
+            # 2) Exact required weight
+            if can_weight == remaining_weight:
+                return [can_weight]
+
+            # 3) Tests with the cans left
+
+            # Removes the can from the list
+            cans_copy = remaining_cans.copy()
+            cans_copy.remove(can_weight)            
+
+            branch_cans = [can_weight] + _bag_recursive(remaining_weight - can_weight, cans_copy)
+
+            # If the weight is greater
+            branch_weight = sum(branch_cans)
+            if branch_weight > max_weight_reached:
+                max_weight_reached = branch_weight
+                max_weight_cans = branch_cans
+
+        return max_weight_cans
+    
+    return _bag_recursive(max_weight, sorted(cans_weight))
+
+
+def greatest_sub_sequence(numbers: list):
+    # Done with brute force - seems like it should be done with dynamic programming
+
+    def _greatest_recursive(current_index, numbers):
+
+        max_count = -1
+        max_branch = []
+        for i in range(current_index + 1, len(numbers)):
+
+            if numbers[i] < numbers[current_index]:
+                continue
+
+            # Greater or equal passes
+            branch = _greatest_recursive(i, numbers)
+            if len(branch) > max_count:
+                max_count = len(branch)
+                max_branch = [numbers[i]] + branch
+
+        return max_branch
+    
+
+    longest = []
+    for i in range(len(numbers)):
+        
+        sub_seq = [numbers[i]] + _greatest_recursive(i, numbers)
+        if len(sub_seq) > len(longest):
+            longest = sub_seq
+
+    return longest
+
+
+def sum_subset(target, numbers):
+
+    for number in numbers:
+
+        # Cuts the branch
+        if number > target:
+            continue
+        
+        # Found - base case
+        if number == target:
+            return True
+
+        remaining_copy = numbers.copy()
+        remaining_copy.remove(number)
+        if sum_subset(target - number, remaining_copy):
+            return True
+
+    return False
---
+++ practicas/tp-ada/code/tests/test_backtracking.py
@@ -0,0 +1,25 @@
+import unittest
+from backtracking import give_change, maximize_bag_weight, greatest_sub_sequence, sum_subset
+
+class BacktrackingTest(unittest.TestCase):
+
+    def test_give_change(self):
+        self.assertEqual(give_change(14, [1, 2, 6, 8, 10]), [8, 6])
+        self.assertEqual(give_change(20, [1, 3, 11, 7, 12]), [12, 7, 1])
+
+    def test_bag(self):
+        self.assertEqual(maximize_bag_weight(22, [30, 17, 7, 8, 9, 6]), [9, 7, 6])
+        self.assertEqual(maximize_bag_weight(22, [30, 17, 8, 9, 6]), [17])
+
+    def test_greatest_sub_sequence(self):
+        self.assertEqual(greatest_sub_sequence([5, 1, 3, 6, 100, 17, 21]), [1, 3, 6, 17, 21])
+
+    def test_sum_subset(self):
+        self.assertEqual(sum_subset(14, [10, 4]), True)
+        self.assertEqual(sum_subset(14, [10, 3]), False)
+        self.assertEqual(sum_subset(15, [8, 6, 7, 5, 3, 10, 9]), True)
+        self.assertEqual(sum_subset(15, [11, 6, 5, 1, 7, 13, 12]), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
